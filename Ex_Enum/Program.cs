06d762f3d9baea6c9271c1941b0a5e1555c21c9f
Michael Silva <michael.steam@outlook.com>
2022-03-08 09:08:18 -0300

    Adicionar arquivos de projeto.

---
+++ Ex_Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ex_Enum
+{
+    internal class Program
+    {
+
+        enum Cor
+        {
+            Vermelho,
+            Verde = 10,
+            Azul
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(nomeDaCor(Cor.Vermelho));
+            Console.WriteLine(nomeDaCor(Cor.Verde));
+            Console.WriteLine(nomeDaCor(Cor.Azul));
+
+
+        }
+
+
+        static string nomeDaCor(Cor c)
+        {
+            switch (c)
+            {
+                case Cor.Vermelho:
+                    return String.Format("Vermelho = {0}", (int)c);
+                case Cor.Verde:
+                    return String.Format("Vermelho = {0}", (int)c);
+                case Cor.Azul:
+                    return String.Format("Vermelho = {0}", (int)c);
+                default:
+                    return "Cor não definida";
+            }
+        }
+
+    }
+}
